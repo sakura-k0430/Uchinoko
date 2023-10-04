f40ae70b30bfe29927f52da18d32fe434e29a3f2
sakura-k0430 <ttsk951031@gmail.com>
2023-10-04 12:37:22 +0000

    ログイン機能完了、ヘッダー作成

--- db/migrate/20231003073836_devise_create_customers.rb
+++ db/migrate/20231003073836_devise_create_customers.rb
@@ -7,8 +7,7 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :name, null: false
-      t.string :address, null: false
-      t.string :phone_number, null: false
+      t.string :address
       t.boolean :is_deleted, null: false, default: false
 
       ## Recoverable
--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,9 @@ ActiveRecord::Schema.define(version: 2023_10_04_071144) do
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.string "address"
+    t.boolean "is_deleted", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
